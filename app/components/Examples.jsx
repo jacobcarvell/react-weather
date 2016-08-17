cacd736c0b721550bd235ab5b03363fe384d8872
Jacob Carvell <jacobcarvell@gmail.com>
2016-08-17 14:30:39 +1000

    Added some styling to the examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,16 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples page of this site!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li><Link to='/?location=Brisbane'>Brisbane, Australia</Link></li>
+          <li><Link to='/?location=London'>London, UK</Link></li>
+          <li><Link to='/?location=Beijing'>Beijing, China</Link></li>
+        </ol>
       </div>
     )
 };
